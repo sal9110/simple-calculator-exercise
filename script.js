b7651f207fb71175115a3d4fd6dc96c52febb72f
Salvo <salvopatti110@gmail.com>
2021-06-12 19:01:20 +0200

    made a very basic calculator

---
+++ script.js
@@ -0,0 +1,36 @@
+
+    const  body = document.querySelector("body")
+    const myButton = document.createElement("button")
+    body.appendChild(myButton).innerHTML = 'calculate'
+
+    const myDiv= document.createElement('div')
+    
+    let result = "-"
+    let resultMessage = ()=>{body.appendChild(myDiv).innerHTML= `The total is ${result}`}
+ 
+    myButton.addEventListener('click', function(event){
+       event.preventDefault()
+       const firstNum = parseInt(document.querySelector('#firstNum').value)
+       const secondNum = parseInt(document.querySelector('#secondNum').value)
+       const operator = document.querySelector('#operator').value
+        if (operator === "+"){
+        result=firstNum + secondNum
+        resultMessage()
+    } else if(operator === "-") {
+        result=firstNum - secondNum
+        resultMessage()
+    } else if ( operator === "*"){
+        result=firstNum * secondNum
+        resultMessage()
+    } else if (secondNum === 0 && operator ==="/"){
+            body.appendChild(myDiv).innerHTML= `Second number cannot be zero`
+            
+    }  else {
+            result = firstNum / secondNum
+            resultMessage()
+        }
+   
+    })
+    
+
+   
